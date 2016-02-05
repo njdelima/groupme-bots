3b1f5b7c416ac7865d1c3f5fbd6aa94fcb6ec52f
Neeraj Joshua DeLima <neeraj.delima@gmail.com>
2016-02-05 16:51:55 -0500

    took out functions

--- snape.php
+++ snape.php
@@ -1,6 +1,11 @@
 <?php
-	// needs an access token, bot id, group id
-	require_once 'neeraj_credentials.php';
+	/*
+	 * This is the bot callback function. All messages from the group
+	 * post to this script. This script decides is the message is a 
+	 * valid command, and does what's needed
+	 */
+
+	require_once 'functions.php';
 
 	$data = json_decode(file_get_contents('php://input'));
 
@@ -10,7 +15,11 @@
 
 	$text = $data->text;
 
-		preg_match('/snape,? (make a|add)? ?saveplate for (.+)/i', $text, $matches);
+	if ($text == "meow" || $text == "Meow") {
+		postMessage("stfu karma whore");
+	}
+
+	preg_match('/snape,? (make a|add)? ?saveplate for (.+)/i', $text, $matches);
 
 	if ($matches != null) {
 		addName($matches[count($matches) - 1]);
@@ -26,49 +35,5 @@
 				listNames();
 			}
 		}
-	}
-	function addName($name) {
-        if ( ( date("N") < 5 ) || ( date("N") == 5 && date("G") < 12 ) ) {
-        	file_put_contents("list", $name . "\n", FILE_APPEND);
-			postMessage("Added saveplate for " . $name . ".");
-        } else {
-        	postMessage("No saveplates until Monday.");
-        }
-	}
-	function removeName($name) {
-		$oldList = file_get_contents("list");
-		$newList = str_ireplace($name . "\n", "", $oldList);
-		if ($newList == $oldList) {
-			postMessage("Sorry! I couldn't find " . $name . " on the list.");
-		} else {
-			file_put_contents("list", $newList);
-			postMessage("Removed saveplate for " . $name . ".");
-		}
-	}
-
-	function listNames() {
-		$meal = date("G") < 12 ? "Lunch" : "Dinner";
-		$message = $meal . " saveplates for " . date("l, jS \of F") . ":\n\n";
-		$message = $message . file_get_contents("list");
-
-		postMessage($message);
-	}
-
-	function postMessage($message) {
-		global $bot_id;
-
-		//build the url
-		$message = urlencode($message);
-		$url = 'https://api.groupme.com/v3/bots/post?bot_id='.$bot_id.'&text='.$message;
-
-		// send the message
-
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, $url);
-		curl_setopt($ch, CURLOPT_POST, true);
-		curl_setopt($ch, CURLOPT_POSTFIELDS, array());
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-		$result = curl_exec($ch);
 	}
 ?>
